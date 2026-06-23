4681793f3a69782b6139019f428cc30e90c4a718
Govindaraj Cholappagol <govindaraj5547@gmail.com>
2026-06-23 16:46:22 +0530

    Installed mongoose and connected to atlas, also created schema

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 // console.log(app.get('env'));
-console.log(process.env);
+// console.log(process.env);
 
-const port = 3000;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+// const DB = process.env.DATABASE_LOCAL;  // Using local database for development
+
+mongoose.connect(DB).then(() => {
+  console.log('DB connection successful!');
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
