f993aa1001727af5b9d2e6dfd4212dc4e941d9e7
Govindaraj Cholappagol <govindaraj5547@gmail.com>
2026-06-23 16:47:39 +0530

    created simple tour schema

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,19 +1,20 @@
-const fs = require('fs');
+// const fs = require('fs');
+const Tour = require('./../models/tourModel');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
-);
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
+// );
 
-exports.checkId = (req, res, next, val) => {
-  console.log(`Tour id : ${val}`);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'Fail',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-};
+// exports.checkId = (req, res, next, val) => {
+//   console.log(`Tour id : ${val}`);
+//   if (req.params.id * 1 > tours.length) {
+//     return res.status(404).json({
+//       status: 'Fail',
+//       message: 'Invalid ID',
+//     });
+//   }
+//   next();
+// };
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
