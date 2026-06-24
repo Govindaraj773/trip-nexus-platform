246a36715d29141a5c8de775c026b68f654e2233
Govindaraj Cholappagol <govindaraj5547@gmail.com>
2026-06-24 23:32:15 +0530

    delete tour using mongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -84,7 +84,6 @@ exports.updateTour = async (req, res) => {
       new: true,
       runValidators: true,
     });
-
     res.status(200).json({
       status: 'success',
       data: {
@@ -99,9 +98,17 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
 };
