380cbadef6359d0b5890c3b511ed51b1eb9f2bd7
Govindaraj Cholappagol <govindaraj5547@gmail.com>
2026-06-24 17:43:10 +0530

    Reading doc / get all tours / get tour using mangoose (Using find and findById methods)

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,61 +1,40 @@
 // const fs = require('fs');
 const Tour = require('./../models/tourModel');
 
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
-// );
-
-// exports.checkId = (req, res, next, val) => {
-//   console.log(`Tour id : ${val}`);
-//   if (req.params.id * 1 > tours.length) {
-//     return res.status(404).json({
-//       status: 'Fail',
-//       message: 'Invalid ID',
-//     });
-//   }
-//   next();
-// };
-
-// exports.checkBody = (req, res, next) => {
-//   if (!req.body.name || !req.body.price) {
-//     return res.status(400).json({
-//       status: 'Fail',
-//       message: 'missing name or price',
-//     });
-//   }
-//   next();
-// };
-
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.json({
-    status: 'success',
-    result: tours.length,
-    requestedTime: req.requestTime,
-    data: {
-      tours,
-    },
-  });
+exports.getAllTours = async (req, res) => {
+  // console.log(req.requestTime);
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      result: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  const id = Number(req.params.id);
-  const tour = tours.find((el) => el.id === id);
-
-  // Check if the tour with the given ID exists
-  // if(id > tours.length){
-  // if (!tour) {
-  //   return res.status(404).json({
-  //     status: 'fail',
-  //     message: 'not found with this id',
-  //   });
-  // }
-  res.json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
@@ -63,7 +42,6 @@ exports.createTour = async (req, res) => {
     // const newTour = new Tour();
     // newTour.save();
     const newTour = await Tour.create(req.body);
-
     res.status(201).json({
       status: 'success',
       data: {
